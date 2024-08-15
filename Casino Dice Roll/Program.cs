2d2ead162678f4db98e2ce722ed0a8d71681d913
Martina Kay Basquez <martinabasquez@gmail.com>
2024-08-14 21:01:54 -0500

    methods are the bane of my existence

---
+++ Casino Dice Roll/Program.cs
@@ -0,0 +1,126 @@
+﻿/*TASK: SIMULATES DICE ROLLING
+
+    BUILD SPECS:
+1. PROMPT USER FOR NUMBER 1-6
+    - EXCEPTION HANDLING
+2. PROMPTS USER TO ROLL DICE
+3. APP ROLLS 2 n-SIDED DICE -> DISPLAY RESULTS + TOTAL
+4. 6-SIDED DICE:
+- "SNAKE EYES" = 1 AND 1
+    - "ACE DEUCE" = 1 AND 2
+    - "BOX CARS" = 6 AND 6
+    - "WIN": TOTAL OF 7 OR 11
+TOTAL OF 7:
+- 6 AND 1
+    - 3 AND 4
+    - 5 AND 2
+TOTAL OF 11:
+- 5 AND 6
+    - "CRAPS" = TOTAL OF 2, 3, 12
+    - TOTAL OF 2:
+- 1 AND 1
+    - TOTAL OF 3:
+- 2 AND 1
+    - TOTAL OF 12:
+5. APP TAKES ALL USER INPUT CORRECTLY
+6. APP LOOPS PROPERLY
+7. COME UP WITH A SET OF WINNING COMBINATIONS FOR OTHER DICE SIZES BESIDES 6
+*/
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// method that generates random numbers 
+static int DiceCombos(int dice)
+{
+    Random rndm = new Random();
+    int roll = rndm.Next(1, dice);
+    return roll;
+}
+
+// method for build spec 2 
+static void DeAngeloTheHelper(int die1, int die2) 
+// did i do more work than i needed to? yes. did i finally figure out how to do ternary operator and was too happy to let it go? yes, absolutely.
+{
+    int dieTotal = die1 + die2;
+    if (dieTotal == 2)
+    {
+        string snakeEyes = die1 == 1 && die1 == die2 ? "You got snake eyes!" : "Shhhhhhh (delete this later )"; 
+        Console.WriteLine(snakeEyes);
+    }
+    else if (dieTotal == 3)
+    {
+        string aceDeuce = (die1 == 1 && die2 == 2) || (die1 == 2 && die2 == 1) ? "You got ace deuce!" : "Shhhhhhh (delete this later )";
+        Console.WriteLine(aceDeuce);
+    }
+    else if (dieTotal == 12)
+    {
+        string boxCars = (die1 == 6) && (die1 == die2) ? "You got box cars!" : "Shhhhhhh (delete this later )";
+        Console.WriteLine(boxCars);
+    }
+}
+
+// method for build spec 3
+static string HighRoller(int die1, int die2)
+{
+    int dieTotal = die1 + die2;
+    if (dieTotal == 7 || dieTotal == 11)
+    {
+        return "You got a win!";
+    }
+
+    if (dieTotal == 2 || dieTotal == 3 || dieTotal == 12)
+    {
+        return "You got craps!";
+    }
+}
+
+// method for asking user if they want to continue 
+static bool KeepGoing()
+{
+    bool userChoice = true;
+    while (true)
+    {
+        Console.WriteLine("Would you like to roll again? (y/n)");
+        string answer = Console.ReadLine();
+        if (answer == "y")
+        {
+            userChoice = true;
+            break;
+        }
+        else if (answer == "n")
+        {
+            userChoice = !true;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid entry. Please choose again.");
+        }
+    }
+    return userChoice;
+}
+
+
+Console.Write("Let's gamble! How many sides do you want your dice to have?  ");
+int pickingSides = 0;
+while (!int.TryParse(Console.ReadLine(), out pickingSides) || (pickingSides <= 1 || pickingSides > int.MaxValue))
+{
+        Console.WriteLine("Sorry, please choose another number.");
+}
+Console.WriteLine();
+Console.WriteLine("Superb! ~~~Roll your dice~~~");
+
+int dice1 = DiceCombos(pickingSides);  
+Console.WriteLine(dice1); // validation delete later
+int dice2 = DiceCombos(pickingSides); 
+Console.WriteLine(dice2); // validation delete later
+
+int diceTotal = dice1 + dice2;
+Console.WriteLine(diceTotal); 
+
+DeAngeloTheHelper(dice1, dice2);
+
+
+
+
+
